10c047efcc03a29c1d9c3ad3f8cde133d2270ec8
Manoj Naik <manoj.naik.4work@gmail.com>
2024-07-30 21:43:50 +0530

    todo app made responsible

--- Web_Devlpmnt/PROJECTS_PRACTICE/TO_DO APP/script.js
+++ Web_Devlpmnt/PROJECTS_PRACTICE/TO_DO APP/script.js
@@ -2,37 +2,35 @@ const input = document.querySelector("input");
 const btn = document.querySelector("button");
 const ulElmt = document.querySelector("ul");
 
-const addTask =()=>{
-    const liVal = input.value;
-    if(input.value === ""){
-        return;
-    }
-    const liElmnt = document.createElement("li");
-    liElmnt.textContent = liVal;
-    ulElmt.appendChild(liElmnt);
-    input.value ="";
+const addTask = () => {
+  const liVal = input.value;
+  if (input.value === "") {
+    return;
+  }
+  const liElmnt = document.createElement("li");
+  liElmnt.textContent = liVal;
+  ulElmt.appendChild(liElmnt);
+  input.value = "";
 
-    const updateBtn = document.createElement("button");
-    updateBtn.style.backgroundColor='yellow';
-    updateBtn.textContent="Update";
-    liElmnt.appendChild(updateBtn);
-    
-    const upDate =()=>{
-        const newLi = prompt("enter the task to update");
-        liElmnt.textContent = newLi;
-    }
-    updateBtn.addEventListener("click",upDate);
-    
-    const delBtn = document.createElement("button");
-    delBtn.style.backgroundColor='red';
-    delBtn.textContent= 'Delete';
-    liElmnt.appendChild(delBtn);
+  const updateBtn = document.createElement("button");
+  updateBtn.style.backgroundColor = "yellow";
+  updateBtn.textContent = "Update";
+  liElmnt.appendChild(updateBtn);
 
-    const deletBtn =()=>{
-        liElmnt.remove();
-    }
-    delBtn.addEventListener("click",deletBtn);
+  const upDate = () => {
+    const newLi = prompt("enter the task to update");
+    liElmnt.textContent = newLi;
+  };
+  updateBtn.addEventListener("click", upDate);
 
+  const delBtn = document.createElement("button");
+  delBtn.style.backgroundColor = "red";
+  delBtn.textContent = "Delete";
+  liElmnt.appendChild(delBtn);
 
-}
-btn.addEventListener("click",addTask);
+  const deletBtn = () => {
+    liElmnt.remove();
+  };
+  delBtn.addEventListener("click", deletBtn);
+};
+btn.addEventListener("click", addTask);
