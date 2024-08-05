23696b41bf8137612f80fb66f404888fccbfe1d6
Manoj Naik <manoj.naik.4work@gmail.com>
2024-08-05 08:41:56 +0530

    vlc stage 3 ui completed

--- Web_Devlpmnt/PROJECTS_PRACTICE/VLC_MEDIA/script.js
+++ Web_Devlpmnt/PROJECTS_PRACTICE/VLC_MEDIA/script.js
@@ -1,2 +1,8 @@
-const volume = document.querySelector(".menu");
+const volume = document.querySelector(".menu-btn");
+const ul = document.querySelector(".menu-items");
 console.log(volume);
+
+const showMenu = () => {
+    ul.style.display = "block";
+}
+volume.addEventListener("click", showMenu);
